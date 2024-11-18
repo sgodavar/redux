50aa0a863a5ef7d5f590d4b5912951e59883f9af
sgodavar <surya.358@gmail.com>
2024-11-18 12:10:52 -0500

    Initial Commit 2

--- redux-playground/src/App.test.js
+++ redux-playground/src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('if the heading is correct', () => {
+  render(<App />);
+  const headingElement = screen.getByText(/Todo's/i);
+});
+
+test('typing into input and displaying todo', () => {
+  render(<App />);
+  const inputElement = screen.getByPlaceholderText(/Add a new todo .../i);
+  const buttonElement = screen.getByRole('button', { name: /Add Todo/i });
+  fireEvent.change(inputElement, { target: { value: 'Send a mail to Dad' } });
+  fireEvent.click(buttonElement);
 });
---
+++ redux-playground/src/app/app.ts
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
+
+const App1 = () => {
+    const [todos, setTodos] = useState([
+    { id: 2, text: 'Visit the mall to get laptop charger', completed: false },
+    {
+        id: 1,
+        text: 'Get a new shoe at the store for tomorrows event',
+        completed: true,
+    },
+    ]);
+
+    const addTodo = (newTodo) => {
+    setTodos([newTodo, ...todos]);
+    };
+
+    const toggleTodo = (id) => {
+    setTodos(
+        todos.map((todo) => {
+        if (todo.id === id) {
+            return {
+            ...todo,
+            completed: !todo.completed,
+            };
+        }
+        return todo;
+        })
+    );
+    };
+
+    const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+    };
+
+    return (
+    <div className="container">
+        <h1 className="center">Todo's</h1>
+        <AddTodo todos={todos} addTodo={addTodo} />
+        <Todos todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </div>
+    );
+};
+
+export default App1;
